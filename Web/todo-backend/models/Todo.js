9ae980a37d23051fdfbfad80289c08d25347010b
FrankyWish <nithintm.tvk@gmail.com>
2025-06-22 20:21:44 +0530

    Delete Web/todo-backend directory

--- Web/todo-backend/models/Todo.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require('mongoose');
-const TodoSchema = new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-    task: String,
-    completed: Boolean
-});
-module.exports = mongoose.model('Todo', TodoSchema);
